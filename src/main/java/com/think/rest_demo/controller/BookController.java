5544b369f2720c3a99b52899ddc46b641af2e3c2
Your Name <you@example.com>
2025-04-30 00:35:27 +0530

    commit

--- src/main/java/com/think/rest_demo/controller/BookController.java
+++ src/main/java/com/think/rest_demo/controller/BookController.java
@@ -2,6 +2,8 @@ package com.think.rest_demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +29,8 @@ import jakarta.persistence.EntityNotFoundException;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private static final Logger bookLogInfo = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     private BookService bookService;
@@ -55,6 +59,8 @@ public class BookController {
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Object> getBook(@PathVariable Long bookid)
     {
+        bookLogInfo.info("User Info Logging is Enabled");
+        bookLogInfo.debug("User Debug Logging is enabled");
         return BookResponseHandler.responseBuilder
         ("Requested Book details are here", HttpStatus.OK, bookService.getBook(bookid));
     }
