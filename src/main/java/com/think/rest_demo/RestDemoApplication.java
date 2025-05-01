12c5d335f78dde5eb837f5d74de69bf98f16659c
Your Name <you@example.com>
2025-05-01 22:17:36 +0530

    commit

--- src/main/java/com/think/rest_demo/RestDemoApplication.java
+++ src/main/java/com/think/rest_demo/RestDemoApplication.java
@@ -6,6 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+
+@OpenAPIDefinition(
+	info = @Info(
+        title = "Vendor & Book API",
+        description = "API documentation for both Vendor and Book services",
+        version = "1.0",
+        termsOfService = "Standard Terms",
+        contact = @Contact(
+            name = "Sayak Pal",
+            url = "http://vendorbookservice.com",
+            email = "contact@vendorbook.com"
+        ),
+        license = @License(
+            name = "Vendor Book App License",
+            url = "http://vendorbookservice.com"
+        )
+    )
+)
+
+
 @SpringBootApplication
 @EntityScan(basePackages = "com.think.rest_demo.model")
 @EnableJpaRepositories(basePackages = "com.think.rest_demo.repository")
